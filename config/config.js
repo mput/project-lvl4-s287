d0590876523af231f8af8171c455cda51b54d13b
mput <putintsev@gmail.com>
2018-08-22 13:20:25 +0300

    DB conif for heroku

--- config/config.js
+++ config/config.js
@@ -12,7 +12,8 @@ module.exports = {
   production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    host: process.env.DATABASE_URL,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     database: 'database_production',
     dialect: 'postgres',
   },
