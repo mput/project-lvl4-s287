332cd817607ecd0f3218691a1e973a8aa8f514a9
mput <putintsev@gmail.com>
2018-08-22 18:53:31 +0300

    Delete unnecessary variables

--- config/config.js
+++ config/config.js
@@ -10,10 +10,6 @@ module.exports = {
     dialect: 'sqlite',
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
     use_env_variable: 'DATABASE_URL',
     database: 'database_production',
     dialect: 'postgres',
