8299077a44aa89fd1a559f1302dcae82ed9bd7ca
mput <putintsev@gmail.com>
2018-08-22 13:37:37 +0300

    Change db config

--- config/config.js
+++ config/config.js
@@ -14,6 +14,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
+    use_env_variable: 'DATABASE_URL',
     database: 'database_production',
     dialect: 'postgres',
   },
